8fd3196de991de5ea368d2dcede61e1ec98887ff
Hao Wang <61251195+Anfulas@users.noreply.github.com>
2024-04-18 18:33:21 +0800

    Update page.tsx

--- src/app/(platform)/openshift/page.tsx
+++ src/app/(platform)/openshift/page.tsx
@@ -30,7 +30,7 @@ const openshift = () => {
       <br></br>
       <h4> Portal</h4>
         <p>https://apic-ptl-portal-web-cp4i.apps.65fc029a1ba90d001e7bf43a.cloud.techzone.ibm.com/flyr/sandbox</p>
-        <p>admin</p>
+        <p>admin/hwang7</p>
         <p>hao1234..</p>
       <br></br>
       <h4> DataPower</h4>
@@ -46,6 +46,20 @@ const openshift = () => {
             Port = 2525;
             Username = 'dd5e7a57095435';
             Password = 'e4a6128548261a';</p>
+      <br></br>
+
+      <h4> Git Repo for Tekton</h4>
+        <p>https://github.ibm.com/Huan-Meng/apic-devops</p>
+      <br></br>
+
+      <h4> RX pre-prod </h4>
+        <p>https://console-openshift-console.apps.neuuaroclstr02.uat.rx.int/</p>
+      <br></br>
+
+      <h4> Windows Remote Server</h4>
+        <p>na4.services.cloud.techzone.ibm.com:37228</p>
+        <p>administrator</p>
+        <p>IBMDem0s</p>
       <br></br>
     </div>
 
---
+++ src/app/(platform)/rx/page.tsx
@@ -0,0 +1,36 @@
+//rafce
+"use client"
+
+import React from 'react'
+import { Stack, TextArea } from '@carbon/react'
+const openshiftmongo = () => {
+  return (
+    <div>
+      <h2>Command</h2>
+      <br></br>
+      <h4>Link</h4>
+      <h5> Send message to eventstream by rest API  </h5>
+      <TextArea labelText="Text Area label"  rows={12} id="text-area-1" 
+      value={`cpd-cp4i.apps.clstr02.dev.rx.int
+      apic-mgmt-admin-cp4i.apps.clstr02.dev.rx.int
+      apic-mgmt-api-manager-cp4i.apps.clstr02.dev.rx.int
+      [ibmuac01@neupljmpsrvin01 ~]$ oc extract secret/apic-mgmt-admin-pass -n cp4i --to=-
+      # email
+      admin@apiconnect.net
+      # password
+      ((Q9iz^6<w!*0of>Ry}^4y}~
+      id - yuanbing pw- Yuan1234!
+      Huan Meng  [2:21 PM]
+      Username - admin
+      Password - N5Jn3UIO1zWe8IozQj41noCPAI5oIJlN`} />
+      <br></br>
+
+      
+
+
+    </div>
+
+    
+  )
+}
+export default openshiftmongo
